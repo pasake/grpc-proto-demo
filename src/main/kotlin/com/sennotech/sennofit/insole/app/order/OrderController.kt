122ad53b3674b5aa1deede39fb5c05b6ca1ac43c
yan <137447345@qq.com>
2019-03-07 15:27:32 +0800

    create order return orderId

--- src/main/kotlin/com/sennotech/sennofit/insole/app/order/OrderController.kt
+++ src/main/kotlin/com/sennotech/sennofit/insole/app/order/OrderController.kt
@@ -28,10 +28,10 @@ class OrderController(
         if (request == null)
             throw SennofitExceptions.RequestIsNull("3fafcef4-b781-45dc-8d4f-e9856641fc59")
 
-        orderService.createOrder(request)
+        var response = orderService.createOrder(request)
 
         responseObserver?.apply {
-            onNext(CreateOrderResponse.getDefaultInstance())
+            onNext(CreateOrderResponse.newBuilder().setOrderId(response).build())
             onCompleted()
         }
     }
